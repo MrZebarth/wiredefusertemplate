fae5e503f0558968454c78f35d62d057c67682d3
MrZebarth <kris.zebarth@ocsb.ca>
2020-01-04 22:49:55 -0500

    Finished Template

--- main.ts
+++ main.ts
@@ -1,6 +1,39 @@
 namespace SpriteKind {
     export const Wire = SpriteKind.create()
 }
+function UpdateCursor () {
+    cursor.top = Math.floor(120 / Ratio) * (cursorPos + 1) - 2
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    WireList[cursorPos] = WireList[cursorPos] - 1
+    if (WireList[cursorPos] < 0) {
+        WireList[cursorPos] = colourList.length - 1
+    }
+    WireSprites[cursorPos].fill(colourList[WireList[cursorPos]])
+    WireSprites[cursorPos].drawRect(0, 0, 160, 5, 15)
+    sprite_list = sprites.allOfKind(SpriteKind.Wire)
+    for (let value of sprite_list) {
+        if (value.top == Math.floor(120 / Ratio) * (cursorPos + 1)) {
+            value.destroy()
+        }
+    }
+    mySprite2 = sprites.create(WireSprites[cursorPos], SpriteKind.Wire)
+    mySprite2.top = Math.floor(120 / Ratio) * (cursorPos + 1)
+})
+function startPhase () {
+    while (wireCount < 3 || wireCount > 6) {
+        wireCount = game.askForNumber("# of wires? (3-6)", 1)
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorPos += 1
+    cursorPos = cursorPos % wireCount
+    UpdateCursor()
+})
+function InitWirePhase () {
+    InitColours()
+    InitCursor()
+}
 function InitCursor () {
     mySprite = img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -16,28 +49,67 @@ function InitCursor () {
     mySprite.drawRect(0, 0, 160, 9, 10)
     mySprite.drawRect(0, 1, 160, 7, 10)
     cursor = sprites.create(mySprite, SpriteKind.Wire)
-    cursor.top = 58
+    cursor.top = Math.floor(120 / Ratio) - 2
+    cursorPos = 0
 }
 function InitColours () {
     colourList = [2, 1, 8, 5, 15]
-    mySprite = img`
+    WireList = []
+    Ratio = wireCount + 1
+    WireSprites = []
+    for (let index = 0; index <= wireCount - 1; index++) {
+        WireList.push(0)
+        mySprite = img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 `
-    mySprite.fill(colourList[0])
-    mySprite2 = sprites.create(mySprite, SpriteKind.Wire)
-    mySprite2.top = 60
+        mySprite.fill(colourList[WireList[index]])
+        mySprite.drawRect(0, 0, 160, 5, 15)
+        WireSprites.push(mySprite)
+        mySprite2 = sprites.create(mySprite, SpriteKind.Wire)
+        mySprite2.top = Math.floor(120 / Ratio) * (index + 1)
+    }
 }
 sprites.onCreated(SpriteKind.Wire, function (sprite) {
     sprite.setFlag(SpriteFlag.Ghost, true)
 })
-let mySprite2: Sprite = null
-let colourList: number[] = []
-let cursor: Sprite = null
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    WireList[cursorPos] = (WireList[cursorPos] + 1) % colourList.length
+    WireSprites[cursorPos].fill(colourList[WireList[cursorPos]])
+    WireSprites[cursorPos].drawRect(0, 0, 160, 5, 15)
+    sprite_list = sprites.allOfKind(SpriteKind.Wire)
+    for (let value of sprite_list) {
+        if (value.top == Math.floor(120 / Ratio) * (cursorPos + 1)) {
+            value.destroy()
+        }
+    }
+    mySprite2 = sprites.create(WireSprites[cursorPos], SpriteKind.Wire)
+    mySprite2.top = Math.floor(120 / Ratio) * (cursorPos + 1)
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorPos += -1
+    if (cursorPos < 0) {
+        cursorPos = wireCount - 1
+    }
+    UpdateCursor()
+})
 let mySprite: Image = null
+let mySprite2: Sprite = null
+let sprite_list: Sprite[] = []
+let WireSprites: Image[] = []
+let colourList: number[] = []
+let WireList: number[] = []
+let cursorPos = 0
+let Ratio = 0
+let cursor: Sprite = null
+let wireCount = 0
+wireCount = 0
+enum phase {start, wire, solve}
+let state:phase=phase.start
+startPhase()
+state += 1
 scene.setBackgroundColor(1)
-InitColours()
-InitCursor()
+InitWirePhase()
